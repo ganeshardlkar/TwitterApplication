36edf6fc7bca5c1e738133a1193b54fd7930038b
ganeshardlkar <ganeshardalkar5@gmail.com>
2023-02-06 17:48:02 +0530

    Added v1 for tweet and hashtag model

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
-
 app.listen(3000, async () => {
     console.log('Server listening on port 3000');
 
@@ -12,15 +9,5 @@ app.listen(3000, async () => {
 
     console.log('Mongodb connected');
 
-    const tweetRepo = new TweetRepository();
-    // const tweet = await tweetRepo.create({content: 'Sample tweet 7'});
-    // console.log(tweet);
-
-    // const comment = await Comment.create({content: 'Comment for sample tweet 6'})
-    // tweet.comments.push(comment);
-    // tweet.save();
-    // console.log(tweet);
-
-    // const tweet = await tweetRepo.getAll(2, 5);
-    // console.log(tweet[0].contentWithEmail);
+    
 });
