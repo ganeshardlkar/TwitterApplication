10f72ad44934ea990fe1c27902d024600b33431f
ganeshardlkar <ganeshardalkar5@gmail.com>
2023-02-09 15:51:46 +0530

    Added toggle like api

--- src/index.js
+++ src/index.js
@@ -20,12 +20,12 @@ app.listen(3000, async () => {
 
     console.log('Mongodb connected');
 
-    const userRepo = new UserRepository();
-    const tweetRepo = new TweetRepository();
-    const tweets = await tweetRepo.getAll(0, 10);
-    const user = await userRepo.getAll();
+    // const userRepo = new UserRepository();
+    // const tweetRepo = new TweetRepository();
+    // const tweets = await tweetRepo.getAll(0, 10);
+    // const user = await userRepo.getAll();
 
-    const likeService = new LikeService();
-    await likeService.toggleLike(tweets[0].id, 'Tweet', user[0].id);
+    // const likeService = new LikeService();
+    // await likeService.toggleLike(tweets[0].id, 'Tweet', user[0].id);
 
 });
