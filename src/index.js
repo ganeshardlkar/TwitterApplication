ea4bd67f2ae4dd0095609eb33361bb2188c300b7
ganeshardlkar <ganeshardalkar5@gmail.com>
2023-02-09 10:39:34 +0530

    Added like model and basic user model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-// const express = require('express');
 import express from 'express';
 import bodyParser from 'body-parser';
-// const connect = require('./config/database');
 import { connect } from './config/database.js';
 import apiRoutes from './routes/index.js';
 
@@ -10,8 +8,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
-
-import service from './services/tweet-service.js';
 
 app.listen(3000, async () => {
     console.log('Server listening on port 3000');
---
+++ src/models/like.js
@@ -0,0 +1,22 @@
+import mongoose, { mongo } from "mongoose";
+
+const likeSchema = new mongoose.Schema({
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet', 'Comment']
+    },
+    likeable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, { timestamps: true });
+
+const Like = mongoose.Model('Like', likeSchema);
+export default Like;
