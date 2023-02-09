5c0d82daf04031e84a3379d09c093dd4a9ad140a
ganeshardlkar <ganeshardalkar5@gmail.com>
2023-02-09 10:54:11 +0530

    Added basic crud repo and extended it in likerepo

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,7 +1,12 @@
 // const Tweet = require('../models/tweet');
 import Tweet from '../models/tweet.js';
+import CrudRepository from './crud-repository.js';
 
-class TweetRepository {
+class TweetRepository extends CrudRepository {
+    constructor() {
+        super(Tweet);
+    }
+
     async create(data) {
         try {
             const tweet = await Tweet.create(data);
@@ -11,14 +16,14 @@ class TweetRepository {
         }
     }
 
-    async get(id) {
-        try {
-            const tweet = await Tweet.findById(id);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    // async get(id) {
+    //     try {
+    //         const tweet = await Tweet.findById(id);
+    //         return tweet;
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
     async getWithComments(id) {
         try {
@@ -29,23 +34,23 @@ class TweetRepository {
         }
     }
 
-    async update(tweetId, data) {
-        try {
-            const tweet = await Tweet.findByIdAndUpdate(tweetId, data, { new: true });
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    // async update(tweetId, data) {
+    //     try {
+    //         const tweet = await Tweet.findByIdAndUpdate(tweetId, data, { new: true });
+    //         return tweet;
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
-    async destroy(id) {
-        try {
-            const tweet = await Tweet.findByIdAndRemove(id);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    // async destroy(id) {
+    //     try {
+    //         const tweet = await Tweet.findByIdAndRemove(id);
+    //         return tweet;
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
     async getAll(offset, limit) {
         try {
